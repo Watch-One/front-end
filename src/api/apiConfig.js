8fa5bcd136d41a4b079b1689d0bac29c4c9d066a
lucaspintos909 <lucaspintos909@gmail.com>
2022-05-12 12:48:37 -0300

    Usando variable del dotenv en vez de dejarlo en texto plano

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,8 @@
+let API_KEY = process.env.REACT_APP_API_KEY;
+
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'Key',
+    apiKey: API_KEY,
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
