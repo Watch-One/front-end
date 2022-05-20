64cf3f39c5f408731325f2e3475bd0c3db368d7a
Lucas Pintos <lucaspintos909@gmail.com>
2022-05-19 23:08:19 -0300

    Arreglando incongruencias con las variables

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import queryString from 'query-string';
 
-import apiConfig from './apiConfig';
+import apiConfigTMDB from './apiConfig';
 
 const axiosClient = axios.create({
-    baseURL: apiConfig.baseUrl,
+    baseURL: apiConfigTMDB.baseUrl,
     headers: {
         'Content-Type': 'application/json'
     },
-    paramsSerializer: params => queryString.stringify({...params, api_key: apiConfig.apiKey})
+    paramsSerializer: params => queryString.stringify({...params, api_key: apiConfigTMDB.apiKey})
 });
 
 axiosClient.interceptors.request.use(async (config) => config);
