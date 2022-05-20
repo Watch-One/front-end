96da0994d7d74027e9a0e48e11c0c3d2676e254a
Lucas Pintos <lucaspintos909@gmail.com>
2022-05-19 23:11:09 -0300

    Obteniendo proveedores y mostrandolos en el detalle de la pelicula

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -4,22 +4,39 @@ import { useParams } from 'react-router';
 import tmdbApi from '../../api/tmdbApi';
 import apiConfig from '../../api/apiConfig';
 
+import {getProviders} from "../../api/apiProviders";
+
 import './detail.scss';
 import CastList from './CastList';
 import VideoList from './VideoList';
 
 import MovieList from '../../components/movie-list/MovieList';
+import MovieProvider from '../../components/movie-provider/MovieProvider';
 
 const Detail = () => {
 
     const { category, id } = useParams();
 
     const [item, setItem] = useState(null);
+    const [providers, setProviders] = useState([]);
 
     useEffect(() => {
         const getDetail = async () => {
             const response = await tmdbApi.detail(category, id, {params:{}});
             setItem(response);
+
+            let title = response.title ? response.title : response.name; 
+            
+            getProviders(title).then(resp => {
+                let array = []
+                let keys = Object.keys(resp.data)
+                
+                keys.forEach(key => {
+                    array.push(resp.data[key])
+                })
+                
+                setProviders(array)
+            })
             window.scrollTo(0,0);
         }
         getDetail();
@@ -47,6 +64,7 @@ const Detail = () => {
                                     }
                                 </div>
                                 <p className="overview">{item.overview}</p>
+                                <p>{providers.map((provider) => <MovieProvider provider={provider}/>)}</p>
                                 <div className="cast">
                                     <div className="section__header">
                                         <h2>Casts</h2>
